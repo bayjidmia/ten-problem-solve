e37ae9a800b934612ab64307ad43ac06bd0f5d0e
bayjidmia <mdbayjidmia982@gmail.com>
2026-01-13 09:52:00 +0600

    making reverseString and countVowels function

---
+++ Problem.js
@@ -0,0 +1,18 @@
+// problem 1
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+// problem 2
+function countVowels(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
